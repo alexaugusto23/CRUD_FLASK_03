3fd3f0e44eeae08e5be0f3a3544016950a7c5e05
alexaugusto23 <contato.alexaugusto@hotmail.com>
2020-10-30 20:19:57 -0300

    Finalização da aplicação de cadastro.

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,7 @@
-tbAlunos_alexsandro_RA_1901705
+airro, estado, complemento,cep):
+        self.ra = ra
+        self.nome_do_aluno = nome_do_aluno
+        self.email_do_aluno = email_do_aluno
+        self.logradouro = logradouro
+        self.numero = numero
+        self.bairro = bairro
