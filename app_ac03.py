6fb84b7f5e444064811df7202e05232c7817bcad
alexaugusto23 <contato.alexaugusto@hotmail.com>
2020-10-30 23:27:41 -0300

    instalando as depedências.

--- app_ac03.py
+++ app_ac03.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify, render_template , redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 import requests
 import json
+import os
 
 app_ac03 = Flask(__name__)
 #app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///cliente.db"
@@ -82,7 +83,8 @@ def delete(id):
 if __name__ == '__main__':
     #db.create_all()
     #db.drop_all()
-    app_ac03.run(debug=True, host='0.0.0.0')
+    port = int(os.environ.get("PORT",5000))
+    app_ac03.run(debug=True, host='0.0.0.0', port=port)
 
 '''
 select * from maap_system
@@ -92,4 +94,8 @@ from app_ac03 import db
 db.create_all()
 db.drop_all() 
 http://localhost:5000/
+heroku
+pip install pipenv
+pipenv install requests
+pipenv check
 '''
--- tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-
-    db.session.commit()
-    return redirect(url_for('index'))
-
-if __name__ == '__main__':
